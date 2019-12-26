e81b6c0bec9bcc3857b2520dd21872bf7eda5a63
Andrzej <andy.ashaman@gmail.com>
2019-12-26 16:21:38 +0100

    second iteration with vue-cli

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const router = express.Router();
+
+// allow fetching data on local default vue dev server
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+  next()
+})
+
+router.get('/api', (req, res, next) => {
+  res.json({ "attendee": "Ana" })
+})
+
+app.use(router)
+
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
+// app.use(express.static(path.join(__dirname, 'test')))
+
+app.listen(process.env.PORT || 5000)
